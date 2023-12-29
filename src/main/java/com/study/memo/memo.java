801cac19baf1ade14403e404acd6fa397dfa4924
dksadasjkl <155143729+dksadasjkl@users.noreply.github.com>
2023-12-29 17:27:28 +0900

    Add files via upload

---
+++ src/main/java/com/study/memo/memo.java
@@ -0,0 +1,20 @@
+package com.study.memo;
+
+public class memo {
+    // F5 파일 카피
+    // ctrl + y 드래그 삭제
+    // ctrl + d 드래그 복사
+
+    // Boolean, String, Integer 등 -> 참조 자료형 - null, true, false 값이 들어감.
+    
+
+
+
+
+
+
+
+
+
+
+}
